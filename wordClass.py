e7847215d1acdd9f18e61ecb97d18cc380d8334a
EdwardG5 <edwardgaremo@gmail.com>
2020-03-09 13:16:43 -0400

    String method added to get the string/pattern represented by that word

--- wordClass.py
+++ wordClass.py
@@ -7,7 +7,7 @@ class Word:
 		self._length = length								    # Length of the word
 		self._constrained = constrained						    # The number of characters linked to other characters
 		self._set = setChars									# The number of characters already set
-		self._chars = ["" for x in range(length+1)] 			# A position for each character in the word + 1 initial blank to allow easy use with depth
+		self._chars = ["-" for x in range(length+1)] 			# A position for each character in the word + 1 initial blank to allow easy use with depth
 		self._pointers = [None for x in range(length+1)]		# Pointers to other word who ith character is linked to word's ith character (pointer to word, index in word)
 		self._indices = [0 for x in range(length+1)]			# Indices corresponding to pointers
 		self._rank = 0											# Rank = order in which the words are evaluated, starting at 1. 0 = uninitialised
@@ -22,6 +22,9 @@ class Word:
 	# def __repr__(self):
     #    string = f"""{self.chars[1:]}, Word {self.index}, Length {self.length}, {self.constrained} constraints with {self.set} set characters"""
     #    return string
+
+	def string(self):
+		return "".join(self.chars[1::])
 
 	def length(self):
 		return self._length
